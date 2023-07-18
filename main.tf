455f367b3d511b2dfccf0431876d38a5fdca2fbc
SaiKrishna <saikrishnareddy9100@gmail.com>
2023-07-17 22:24:36 -0300

    diskrepo

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.rgname
+  location = var.loc
+}
+
+resource "azurerm_managed_disk" "example" {
+  name                 = "disk1"
+  location             = "West US"
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+
+}
